60c29fce5257086ecf390385917cf05e40330b16
Julian López Salvucci <julian.lopez@asociart.com.ar>
2023-08-19 20:10:53 -0300

    incorporar form al resume componente

--- Frontend/app/src/app/app.module.ts
+++ Frontend/app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppLayoutModule } from './layout/app.layout.module';
+import { SharedModule } from 'src/shared/shared/shared.module';
+import { ResumeComponent } from './demo/pages/resume/resume.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [AppRoutingModule, AppLayoutModule],
+  declarations: [AppComponent, ResumeComponent],
+  imports: [AppRoutingModule, AppLayoutModule, SharedModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- Frontend/app/src/app/demo/pages/resume/resume.component.ts
+++ Frontend/app/src/app/demo/pages/resume/resume.component.ts
@@ -1,7 +1,25 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
 })
-export class ResumeComponent {}
+export class ResumeComponent {
+  constructor(private fb: FormBuilder) {}
+
+  registerForm = this.fb.group({
+    id: [0],
+    nombre: [''],
+    apellidos: [''],
+    correoElectronico: [''],
+    telefono: [''],
+    dirección: [''],
+    codigoPostal: [''],
+    localidad: [''],
+  });
+
+  register() {
+    console.log(this.registerForm.value);
+  }
+}
