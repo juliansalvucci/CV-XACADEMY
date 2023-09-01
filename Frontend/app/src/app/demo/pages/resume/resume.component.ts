55830706eb1f2c65dfd8ddbbb02f5b86e6e97cbb
Julian López Salvucci <julian.lopez@asociart.com.ar>
2023-09-01 14:04:03 -0300

    .

--- Frontend/app/src/app/demo/pages/resume/resume.component.ts
+++ Frontend/app/src/app/demo/pages/resume/resume.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
+import { IExperience } from 'src/app/interfaces/IExperience';
 import { curriculumService } from 'src/app/services/resume/curriculum.service';
 
 @Component({
@@ -10,13 +11,18 @@ import { curriculumService } from 'src/app/services/resume/curriculum.service';
 export class ResumeComponent {
   constructor(private fb: FormBuilder, private service: curriculumService) {}
 
+  private experiences : IExperience[] | undefined
+
   resumeForm = this.fb.group({
-    id: [0],
     firstName: [''],
     lastName: [''],
-    contactEmail: [''],
+    contactEmail: ['', [Validators.email]],
     contactPhone: [''],
     photoUrl: [''],
+    experiences: this.fb.array([]), // Debes definir esto para representar la lista de experiencias.
+    projects: this.fb.array([]), // Debes definir esto para representar la lista de proyectos.
+    skills: this.fb.array([]), // Debes definir esto para representar la lista de habilidades.
+    educations: this.fb.array([]), // Debes definir esto para representar la lista de educaciones.
   });
 
   educationForm = this.fb.group({
@@ -48,12 +54,24 @@ export class ResumeComponent {
   skillForm = this.fb.group({
     skillName: [''],
     resumeId: [''],
-  })
+  });
+
+  agregarExperiencia() {
+    const experiencesArray = this.resumeForm.get('experiences') as FormArray;
+    const newExperienceForm = this.experiences; // Crear una nueva instancia del formulario de experiencia
+    experiencesArray.push(newExperienceForm);
+  }
+  
 
   register() {
+    console.log(this.experienceForm);
+    
+    this.agregarExperiencia();
     console.log(this.resumeForm.value);
+    /*
     this.service.alta(this.resumeForm.value).subscribe(() => {
       console.log('exito');
     });
+    */
   }
 }
