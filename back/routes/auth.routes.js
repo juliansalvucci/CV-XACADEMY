000ebbd55bd70c03bf37aafb796be2c01d1e073d
lucaspaglia3 <lucaspaglia32@gmail.com>
2023-10-13 18:52:47 -0300

    login también devuelve el userId

--- back/routes/auth.routes.js
+++ back/routes/auth.routes.js
@@ -8,10 +8,11 @@ router.post( '/', async (req, res) => {
     const { user, password } = req.body;
     const userFound = await userService.validateUser({ user, password });
     if(userFound) {
+        const userId = await userService.getUserId({ user });
         const token = jwt.sign({ user }, SERVER_SECRET, {
             expiresIn: '50m',
         });
-        return res.json({ token });
+        return res.json({ token, userId });
     }
     res.status(401).json({ error: 'Invalid User'});
 });
